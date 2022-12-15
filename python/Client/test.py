6145128adc1507f0440feeb2a5512bc207729c2f
nannan1001 <534130658@qq.com>
2022-12-15 11:06:24 -0500

    Add try catch to deal with not started server

--- python/Client/test.py
+++ python/Client/test.py
@@ -8,6 +8,7 @@ class Client_Tests(unittest.TestCase):
     Test using mock client
     '''
     def test01(self):
+        print("Test1: Using Mock Client\n")
         InventoryClientMock = InventoryClient()
         InventoryClientMock.getBookTitles = MagicMock(return_value = ["Happy Coding", "Take Photos"])
         InventoryClientMock.getBookTitles(["1", "2"], key = "value")
@@ -18,10 +19,15 @@ class Client_Tests(unittest.TestCase):
     Test using real server
     '''
     def test02(self):
-        InventoryClientReal = InventoryClient()
-        res = get_book_titles(InventoryClientReal, ["1", "2"])
-        exp = ["Happy Coding", "Take Photos"]
-        self.assertEqual(res, exp)
+        print("\n")
+        print("Test2: Using Real Server\n")
+        try:
+            InventoryClientReal = InventoryClient()
+            res = get_book_titles(InventoryClientReal, ["1", "2"])
+            exp = ["Happy Coding", "Take Photos"]
+            self.assertEqual(res, exp)
+        except:
+            print("Server Not Started\n")
         
 if __name__ == '__main__':
     unittest.main()
